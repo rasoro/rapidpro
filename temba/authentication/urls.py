bc071f0f20f22b6648411b99aecb240a386dadf9
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2020-03-13 04:04:14 -0300

    Added urls for two factor authentication

--- temba/authentication/urls.py
+++ temba/authentication/urls.py
@@ -1,5 +1,15 @@
+from two_factor.views import BackupTokensView, DisableView, ProfileView, QRGeneratorView, SetupCompleteView, SetupView
+
 from django.conf.urls import url
 
 from .views import Login
 
-urlpatterns = [url(r"^$", Login.as_view(), name="authentication.login")]
+urlpatterns = [
+    url(r"^login/$", Login.as_view(), name="authentication.login"),
+    url(r"^two_factor/setup/$", SetupView.as_view(), name="authentication.setup"),
+    url(r"^two_factor/qrcode/$", QRGeneratorView.as_view(), name="authentication.qr"),
+    url(r"^two_factor/setup/complete/$", SetupCompleteView.as_view(), name="authentication.setup_complete"),
+    url(r"^two_factor/backup/tokens/$", BackupTokensView.as_view(), name="authentication.backup"),
+    url(r"^two_factor/profile/$", ProfileView.as_view(), name="authentication.profile"),
+    url(r"^two_factor/disable/$", DisableView.as_view(), name="authentication.disable"),
+]
