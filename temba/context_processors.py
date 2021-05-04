2619072ba4157ca38074e56d8f7500ea96f25b85
Joao <jcbalmeida@gmail.com>
2021-05-04 14:45:58 -0300

    Adds weni-related context processors into temba.context_processors

--- temba/context_processors.py
+++ temba/context_processors.py
@@ -20,3 +20,25 @@ def analytics(request):
         intercom_app_id=settings.INTERCOM_APP_ID,
         google_tracking_id=settings.GOOGLE_TRACKING_ID,
     )
+
+
+def enable_weni_layout(request):
+
+    host = request.get_host().split(":")[0]
+
+    return {"use_weni_layout": host.endswith(settings.WENI_DOMAINS["weni"])}
+
+
+def weni_announcement(request):
+    return {
+        "announcement_left": settings.ANNOUNCEMENT_LEFT,
+        "announcement_right": settings.ANNOUNCEMENT_RIGHT,
+        "announcement_link": settings.ANNOUNCEMENT_LINK,
+        "announcement_button": settings.ANNOUNCEMENT_BUTTON,
+    }
+
+
+def hotjar(request):
+    return {
+        "hotjar_id": settings.HOTJAR_ID
+    }
